107f8156697142e9e40da080eb343a25a43f8263
daydreamboy <wesley4chen@gmail.com>
2023-11-09 14:36:39 +0800

    add HelloWatchApp

---
+++ HelloWatchApp/HelloWatchOnlyApp/HelloWatchOnlyApp Watch App/HelloWatchOnlyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWatchOnlyAppApp.swift
+//  HelloWatchOnlyApp Watch App
+//
+//  Created by wesley_chen on 2023/11/9.
+//
+
+import SwiftUI
+
+@main
+struct HelloWatchOnlyApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
