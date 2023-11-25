9a765ee9f3dc2a2a11a3f9705da3f43e22aeb675
daydreamboy <wesley4chen@gmail.com>
2023-11-25 19:28:58 +0800

    HelloSwiftUI: AppTestTemplate

---
+++ HelloSwiftUI/AppTestTemplate/AppTestTemplate/AppTestTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTestTemplateApp.swift
+//  AppTestTemplate
+//
+//  Created by wesley_chen on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTestTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
