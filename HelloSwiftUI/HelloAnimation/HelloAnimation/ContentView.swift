28a4094b8e3589e99bb9a7e7b896f5d5b60409a5
daydreamboy <wesley4chen@gmail.com>
2024-01-07 22:15:56 +0800

    HelloSwiftUI: add HelloAnimation

---
+++ HelloSwiftUI/HelloAnimation/HelloAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloAnimation
+//
+//  Created by wesley_chen on 2024/1/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloSwiftUI/HelloAnimation/HelloAnimation/HelloAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAnimationApp.swift
+//  HelloAnimation
+//
+//  Created by wesley_chen on 2024/1/7.
+//
+
+import SwiftUI
+
+@main
+struct HelloAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
