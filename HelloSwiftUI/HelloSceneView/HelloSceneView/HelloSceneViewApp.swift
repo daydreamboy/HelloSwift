51939323397a0101d70c655f675e8c7b1c9ad6a6
daydreamboy <wesley4chen@gmail.com>
2024-08-21 14:13:55 +0800

    HelloSwiftUI: HelloSceneView

---
+++ HelloSwiftUI/HelloSceneView/HelloSceneView/HelloSceneViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSceneViewApp.swift
+//  HelloSceneView
+//
+//  Created by wesley_chen on 2024/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloSceneViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
