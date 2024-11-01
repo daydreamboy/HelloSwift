8581302f57a4c6209515e1e80ddb37e836f1e22a
daydreamboy <wesley4chen@gmail.com>
2024-11-01 17:43:49 +0800

    add HelloStaticFramework

---
+++ HelloFrameworks/HelloStaticFramework/HelloStaticFramework/HelloStaticFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelloStaticFramework.h
+//  HelloStaticFramework
+//
+//  Created by wesley_chen on 2024/11/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloStaticFramework.
+FOUNDATION_EXPORT double HelloStaticFrameworkVersionNumber;
+
+//! Project version string for HelloStaticFramework.
+FOUNDATION_EXPORT const unsigned char HelloStaticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloStaticFramework/PublicHeader.h>
+
+
